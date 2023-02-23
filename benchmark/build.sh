245f67400983cbaf0a914c08732f93e30eef1f24
vpa1977 <vladimir.petko@canonical.com>
2023-02-23 21:53:03 +1300

    liberty deployment

--- benchmark/build.sh
+++ benchmark/build.sh
@@ -4,9 +4,15 @@ ACMEAIR_REPO=https://github.com/vpa1977/${PROJECT}
 
 git clone $ACMEAIR_REPO || \
     (cd ${PROJECT} && \
-     git pull &&
-     git reset --hard)
+     git pull)
 
-cd ${PROJECT}
+pushd ${PROJECT}
 
 mvn package
+
+cp target/*.war ../acmeair-liberty/
+cp target/*.war ../acmeair-tomcat/
+
+popd
+
+docker build -t acmeair-liberty acmeair-liberty
