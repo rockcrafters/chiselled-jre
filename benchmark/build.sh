701a46d95e16cb4bbedd31cd8cb7dff433e62473
Vladimir Petko <vladimir.petko@canonical.com>
2023-03-06 08:46:25 +1300

    remove liberty deployment (#88)

--- benchmark/build.sh
+++ benchmark/build.sh
@@ -15,15 +15,12 @@ mvn clean package -Pexternal-tomcat
 cp target/acmeair-java-2.0.0-SNAPSHOT.war ../acmeair-tomcat/
 cp target/acmeair-java-2.0.0-SNAPSHOT.war ../acmeair-tomcat-corretto/
 mvn -DskipTests=true  package -Pexternal-liberty
-cp target/acmeair-java-2.0.0-SNAPSHOT.jar ../acmeair-liberty/
 cp target/acmeair-java-2.0.0-SNAPSHOT.jar ../acmeair-standalone-chisel/
 cp target/acmeair-java-2.0.0-SNAPSHOT.jar ../acmeair-standalone-temurin/
 cp target/acmeair-java-2.0.0-SNAPSHOT.jar ../acmeair-standalone-corretto/
 
 popd
 
-docker build -t acmeair-liberty acmeair-liberty
-docker build -t acmeair-cliberty acmeair-cliberty
 docker build -t acmeair-tomcat acmeair-tomcat
 docker build -t acmeair-ctomcat acmeair-ctomcat
 docker build -t acmeair-tomcat-corretto acmeair-tomcat-corretto
